9136f001cb2a912a57d61b65bc85c6d4ea21dc50
HeFidelis <heitorfidelis81@gmail.com>
2026-02-21 20:35:41 -0400

    Criando a página inicial de autenticação

---
+++ src/app/landingpage/landingpage.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Profile } from './profile.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-landingpage',
+  standalone: false,
+  templateUrl: './landingpage.html',
+  styleUrl: './landingpage.scss',
+})
+export class Landingpage {
+
+  profile: Profile | undefined;
+
+  constructor(private router: Router){}
+
+  navegar(){
+    this.router.navigate(['/paginas/galeria']);
+  }
+
+  logarComGoogle(){
+
+  }
+
+  isLoggedIn() : boolean {
+    return !!this.profile;
+  }
+}
